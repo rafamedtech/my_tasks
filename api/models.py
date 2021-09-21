9bad6345237d9529b491dde79725a3675c7abeac
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-09-21 13:03:46 -0700

    task and user models created

--- api/models.py
+++ api/models.py
@@ -3,12 +3,12 @@ import uuid
 
 # Create your models here.
 class Task(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000, null=True)
     completed = models.BooleanField(default=False, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
